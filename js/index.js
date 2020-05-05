5f4ffcb5792f9cdbc9a2f48325c6719838dd5633
lateralcreativity <lateralcreativity@outlook.com>
2020-05-05 13:00:10 -0500

    Added fade animation to variables.less and setup eventlistener in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// hover fade animation added to all links
+
+// ----------------- mouseover begins -----------------
+const links = document.querySelectorAll('a');
+links.forEach(x => x.addEventListener('mouseover', event => {
+    x.style.animation = 'fade 1s infinite';
+}));
+links.forEach(x => x.addEventListener('mouseout', event => {
+    x.style.animation = 'none';
+}));
+// ----------------- mouseover ends -----------------
