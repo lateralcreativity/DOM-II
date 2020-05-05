a03724d6f2f88d449669e076effba36120914c5f
lateralcreativity <lateralcreativity@outlook.com>
2020-05-05 15:02:53 -0500

    Holding escape activates rainbow text letting go deactivates it, double clicking images blurs or unblurs depending on status

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 // hover fade animation added to all links
 
-// ----------------- mouseover begins -----------------
+// ----------------- mouseover & mouseout begin -----------------
 const links = document.querySelectorAll('a');
 links.forEach(x => x.addEventListener('mouseover', event => {
     x.style.animation = 'fade 1s infinite';
@@ -9,4 +9,34 @@ links.forEach(x => x.addEventListener('mouseover', event => {
 links.forEach(x => x.addEventListener('mouseout', event => {
     x.style.animation = 'none';
 }));
-// ----------------- mouseover ends -----------------
+// ----------------- mouseover & mouseout end -----------------
+
+
+// ----------------- keydown & keyup begin -----------------
+const paragraphs = document.querySelectorAll('p');
+window.addEventListener('keydown', event => {
+    if(event.keyCode === 27){
+        paragraphs.forEach(x => x.style.animation = 'rainbow 5s infinite');
+    }
+});
+window.addEventListener('keyup', event => {
+    if(event.keyCode === 27){
+        paragraphs.forEach(x => x.style.animation = 'none');
+    }
+});
+// ----------------- keydown & keyup end -----------------
+
+
+// ----------------- dblclick begins -----------------
+const images = document.querySelectorAll('img');
+let blur = false;
+images.forEach(x => x.addEventListener('dblclick', event => {
+    blur = !blur;
+    if(blur){
+        x.style.filter = 'blur(4px)';
+    }
+    if(!blur){
+        x.style.filter = 'none';
+    }
+}));
+// ----------------- dblclick ends -----------------
